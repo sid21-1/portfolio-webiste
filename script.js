01505d7cf279f2ca4f1215d3d609ea274c3197b9
Siddhanth Gupta <guptasiddhanth2@gmail.com>
2023-11-29 19:41:39 +0530

    made some changes in the layout

--- script.js
+++ script.js
@@ -13,36 +13,3 @@ document.addEventListener("DOMContentLoaded", () => {
     }, 500);
   }, 1500);
 });
-
-// Confetti-Container
-document.addEventListener("DOMContentLoaded", function () {
-  const confettiContainer = document.querySelector(".confetti-container");
-  const maxParticles = 50;
-  let isCreatingParticles = true;
-
-  for (let i = 0; i < maxParticles; i++) {
-    createConfetti();
-  }
-
-  function createConfetti() {
-    const confetti = document.createElement("div");
-    confetti.classList.add("confetti");
-
-    confetti.style.left = `${Math.random() * 100}vw`;
-    confetti.style.animationDuration = `${Math.random() * 4 + 2}s`;
-
-    confettiContainer.appendChild(confetti);
-
-    confetti.addEventListener("animationiteration", () => {
-      if (isCreatingParticles) {
-        confetti.style.left = `${Math.random() * 100}vw`;
-        confetti.style.animationDuration = `${Math.random() * 4 + 2}s`;
-      }
-    });
-  }
-
-  // Set a flag to stop creating particles after the initial set.
-  setTimeout(() => {
-    isCreatingParticles = false;
-  }, 2000); // Adjust the timing based on your animation duration
-});
